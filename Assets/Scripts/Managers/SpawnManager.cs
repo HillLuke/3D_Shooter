4419f78901dec00392f3c290efb6e2d9af9bb086
HillLuke <lukehilldev@gmail.com>
2020-09-07 22:07:10 +0100

    Add spawn area, spawn script and test spawn script.

--- Assets/Scripts/Managers/SpawnManager.cs
+++ Assets/Scripts/Managers/SpawnManager.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public Transform SpawnArea;
+    public GameObject PlayerPrefab;
+    public GameObject Player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        Vector3 origin = SpawnArea.position;
+        Vector3 range = SpawnArea.localScale / 2.0f;
+        Vector3 randomRange = new Vector3(Random.Range(-range.x, range.x),
+                                          Random.Range(-range.y, range.y),
+                                          Random.Range(-range.z, range.z));
+        Vector3 randomCoordinate = origin + randomRange;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Player = Instantiate(PlayerPrefab, randomCoordinate, Quaternion.identity);
     }
 }
