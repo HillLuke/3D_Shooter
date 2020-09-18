9daed19f0c3e71d534f523c81195f48048b903f8
HillLuke <lukehilldev@gmail.com>
2020-09-18 14:55:55 +0100

    Add navigation mesh so enemies can find and follow player. Sound effects for guns and some collision improvements.

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
@@ -13,39 +14,91 @@ public class EnemyController : MonoBehaviour
     [SerializeField] private float m_StickToGroundForce = 10f;
 
     private CharacterController m_CharacterController;
-    private Vector3 m_MoveDir;
     private CollisionFlags m_collisionFlags;
+    private NavMeshAgent m_agent;
+    private bool m_setup;
 
     void Start()
     {
         m_CharacterController = GetComponent<CharacterController>();
         Animator = GetComponent<Animator>();
         Health = GetComponent<Health>();
+        m_agent = GetComponent<NavMeshAgent>();
 
         Health.onDie += OnDie;
+
+        m_agent.enabled = false;
+        m_agent.updatePosition = false;
+        m_agent.updateRotation = false;
+        m_setup = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (m_CharacterController.isGrounded && !m_setup)
+        {
+            m_setup = true;
+            m_agent.enabled = true;
+        }
 
+        if (!m_setup)
+        {
+            m_collisionFlags =  m_CharacterController.Move(Physics.gravity * m_gravityMultiplier * Time.fixedDeltaTime);
+            m_agent.enabled = false;
+        }
+
+        if (m_setup)
+        {
+            m_agent.updateRotation = true;
+            m_agent.updatePosition = true;
+            m_agent.destination = (Vector3)(PlayerController.Instance?.gameObject.transform.position);
+            FaceTarget(m_agent.destination);
+            //m_collisionFlags = m_CharacterController.Move(m_agent.desiredVelocity.normalized * m_agent.speed * Time.deltaTime);
+            //m_agent.velocity = m_CharacterController.velocity;
+        }
+
+
+
+        //m_CharacterController.Move(m_agent.desiredVelocity.normalized * m_agent.speed * Time.deltaTime);
+        //m_agent.velocity = m_CharacterController.velocity;
+    }
+
+    private void FaceTarget(Vector3 destination)
+    {
+        Vector3 lookPos = destination - transform.position;
+        lookPos.y = 0;
+        Quaternion rotation = Quaternion.LookRotation(lookPos);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 20f);
     }
 
     void FixedUpdate()
     {
+        //if (m_CharacterController.isGrounded)
+        //{
+        //    m_MoveDir.y = -m_StickToGroundForce;
+        //}
+        //else
+        //{
+        //    m_agent.enabled = false;
+        //    m_MoveDir += Physics.gravity * m_gravityMultiplier * Time.fixedDeltaTime;
+        //}
+        //m_collisionFlags = m_CharacterController.Move(m_MoveDir * Time.fixedDeltaTime);
+    }
+
+    void MoveToPlayer()
+    {
         if (m_CharacterController.isGrounded)
         {
-            m_MoveDir.y = -m_StickToGroundForce;
+            m_agent.destination = (Vector3)(PlayerController.Instance?.gameObject.transform.position);
         }
-        else
-        {
-            m_MoveDir += Physics.gravity * m_gravityMultiplier * Time.fixedDeltaTime;
-        }
-        m_collisionFlags = m_CharacterController.Move(m_MoveDir * Time.fixedDeltaTime);
     }
 
     void OnDie()
     {
+        m_CharacterController.detectCollisions = false;
+        m_CharacterController.enabled = false;
+        m_agent.isStopped = true;
         Animator.SetBool("Death", true);
         StartCoroutine(Death());
     }
